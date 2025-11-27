654fee1e3928868f3aa3fde6b8e796c809273d73
enryloubet <enry.loubet@edu.univ-eiffel.fr>
2025-11-27 19:13:09 +0100

    les tests stylés

--- test_shape.py
+++ test_shape.py
@@ -1,25 +1,31 @@
 import shapefile
 from fltk import *
-sf = shapefile.Reader("departements-20180101") #ouverture du fichier shapefile
-sf.records()# visualisation de toutes les entrées du fichier
 
 
-france=[]
-for i in range(len(sf.records())):    
-    e = sf.shape(i).points
-    
-    for j in range(len(e)):
-
-        x = e[j][0] *20
-        y = e[j][1] *20
-        z=(x,y)
-        france.append(z)
-    
-
+sf = shapefile.Reader("departements-20180101")
 
 cree_fenetre(1200, 1200)
 
-polygone(france)
+
+for shape_rec in sf.shapes():
+    
+    pts = shape_rec.points
+    parts = list(shape_rec.parts) + [len(pts)]
+
+    
+    for i in range(len(parts) - 1):
+        start = parts[i]
+        end = parts[i + 1]
+        segment = pts[start:end]
+
+        
+        poly = []
+        for x, y in segment:
+            X = x * 20 + 100
+            Y = 1200 - (y * 20)
+            poly.append((X, Y))
+
+        polygone(poly)
 
 attend_ev()
 ferme_fenetre()
