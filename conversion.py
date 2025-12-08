4d579fc624e263df4e5e6cc9e89a4ac923b55f07
SamiSouyad <sami.souyad@edu.univ-eiffel.fr>
2025-12-08 11:18:30 +0100

    conversion modif

--- conversion.py
+++ conversion.py
@@ -1,22 +1,20 @@
+
 import math
 
-
-def wsg84_vers_mercator(lon,lat):
+def conv(lon, lat):
     
-    
-    # Rayon de la Terre en mètres
     R = 6378137.0
+    
+    if lat > 85:
+        lat = 85
+    elif lat < -85:
+        lat = -85
 
-    # Conversion des degrés en radians
     lon_rad = math.radians(lon)
     lat_rad = math.radians(lat)
     
-
-    # Calcul des coordonnées Mercator
     x = R * lon_rad
     y = R * math.log(math.tan(math.pi / 4 + lat_rad / 2))
-
-    return (x, y)
-
-
+    
+    return x, y
 
